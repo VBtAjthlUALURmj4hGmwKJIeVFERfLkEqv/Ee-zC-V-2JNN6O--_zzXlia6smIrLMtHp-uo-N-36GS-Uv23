1419f309b44fce39594dced904efe5facf55e855
VBtAjthlUALURmj4hGmwKJIeVFERfLkEqv <unoofficial648@gmail.com>
2025-12-16 14:53:33 +0800

    Update Lua script URL for loading

--- .lua
+++ .lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/5faf94e2cdcf042ebe268a07c216fec197e031df7b5e5e48c6c4a2be2c2dd723/download"))()
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/9480ae72fb569d7366d96e1a41d1fd3925171bf5931f68cbe570acd3e6859a66/download"))()
