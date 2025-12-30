4358ad729c75dba3f556d8be74f963e488d87272
VBtAjthlUALURmj4hGmwKJIeVFERfLkEqv <unoofficial648@gmail.com>
2025-12-30 13:53:00 +0800

    Update Lua script URL to new endpoint

--- .lua
+++ .lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/9480ae72fb569d7366d96e1a41d1fd3925171bf5931f68cbe570acd3e6859a66/download"))()
+loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/f61dc46e23a1abd3008dce2df6dc64ba93c81668b2247290469edee38771d5dc/download"))()
