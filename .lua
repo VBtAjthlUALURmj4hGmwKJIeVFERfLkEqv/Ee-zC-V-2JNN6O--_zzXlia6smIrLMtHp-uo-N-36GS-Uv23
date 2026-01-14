8c187e3968ccc170c15ab2f126f820ec648feae5
VBtAjthlUALURmj4hGmwKJIeVFERfLkEqv <unoofficial648@gmail.com>
2026-01-14 11:24:53 +0800

    Update .lua

--- .lua
+++ .lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.junkie-development.de/api/v1/luascripts/public/f61dc46e23a1abd3008dce2df6dc64ba93c81668b2247290469edee38771d5dc/download"))()
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/a071936007dba890462ac8f19728f717d1d387bcea83c470d80ef501ad729ac5/download"))()
